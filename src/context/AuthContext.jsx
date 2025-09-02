cf5a47d42949d5467d9fe939331bfd099fe357c8
Focusjay <justinchimex52@gmail.com>
2025-09-02 12:16:35 +0100

    small update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -16,6 +16,6 @@ export const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
+// export const useAuth = () => {
+//   return useContext(AuthContext);
+// };
